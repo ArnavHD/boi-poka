2b6fa681ec89d880df24269369a25e0e13f4dbb1
ArnavHD <arnavpaul180@gmail.com>
2025-11-30 11:17:21 -0600

    Two ways of fetch data

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
-    const [allBooks, setAllBooks] = useState([]);
+  const [allBooks, setAllBooks] = useState([]);
 
-    // way-1 of loading data
-    useEffect(()=>{
-        fetch('booksData.json')
-        .then(res=>res.json())
-        .then(data=>{
-            setAllBooks(data);
-        })
-    })
-    return (
-        <div>
-            <h1>This is books</h1>
-        </div>
-    );
+  // way-1 of loading data
+  // useEffect(()=>{
+  //     fetch('booksData.json')
+  //     .then(res=>res.json())
+  //     .then(data=>{
+  //         setAllBooks(data);
+  //     })
+  // })
+
+  // way-2
+  const bookPromise = fetch("./booksData.json").then((res) => res.json());
+  console.log(bookPromise);
+  return (
+    <div className=" text-3xl text-center p-6">
+      <h1>This is books</h1>
+      <Suspense fallback={<span>Loading</span>}>
+        <Book bookPromise={bookPromise}></Book>
+      </Suspense>
+    </div>
+  );
 };
 
 export default Books;
