342688178a263fa4a3398bf3af1b40ec271708df
ArnavHD <arnavpaul180@gmail.com>
2025-11-30 11:43:21 -0600

    Books shown

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,13 +1,31 @@
-import React, { use } from "react";
+import React from "react";
 
 const Book = ({ book }) => {
   //   const data = use(bookPromise);
   //   console.log(data);
 
-  console.log(book)
+  console.log(book);
+
+  const { bookName, author, image , } = book;
   return (
-    <div>
-      <h1>Just book</h1>
+    <div className="card bg-base-100  shadow-sm">
+      <figure className="px-18 py-10">
+        <img
+          src={image}
+          alt="Shoes"
+          className="rounded-xl w-2/3 h-2/3"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Card Title</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect, useState } from "react";
 import Book from "../Book/Book";
 
 const Books = ({ data }) => {
-  const [allBooks, setAllBooks] = useState([]);
+//   const [allBooks, setAllBooks] = useState([]);
 
   // way-1 of loading data
   // useEffect(()=>{
@@ -21,9 +21,11 @@ const Books = ({ data }) => {
     <div className=" text-3xl text-center p-6">
       <h1>This is books</h1>
       <Suspense fallback={<span>Loading</span>}>
-        {
-            data.map((book)=> <Book key={book.bookId} book={book}></Book>)
-        }
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.map((book) => (
+            <Book key={book.bookId} book={book}></Book>
+          ))}
+        </div>
       </Suspense>
     </div>
   );
