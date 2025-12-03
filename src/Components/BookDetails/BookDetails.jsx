92ba1c34f4918753cf6f6b29955d79ce8da1c7aa
ArnavHD <arnavpaul180@gmail.com>
2025-12-02 19:57:24 -0600

    Done

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
 import { addToStoreDB } from "../../utility/addToDB";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+  import { ToastContainer, toast } from "react-toastify";
 
 const BookDetails = () => {
   const { id } = useParams(); // এই id  আছে string এ। তাই একে int এ convert করতে হবে।
@@ -17,6 +22,13 @@ const BookDetails = () => {
     // array or collection আকারে রাখব।
     // if book already exists then show an alert
     // if book doesn't exists then push in the collection array
+    MySwal.fire({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success",
+    });
+
+    toast("Wow so easy!");
     addToStoreDB(id);
   };
 
@@ -25,12 +37,14 @@ const BookDetails = () => {
     <div>
       <img className=" w-48" src={image} alt="" />
       <h5>{bookName}</h5>
+      <ToastContainer />
       <button
-        onClick={()=> handleMarkAsRead(id)}
+        onClick={() => handleMarkAsRead(id)}
         className="btn btn-active btn-success m-2"
       >
         Mark as Read
       </button>
+
       <button className="btn btn-active  btn-info m-2">Wishlist</button>
     </div>
   );
